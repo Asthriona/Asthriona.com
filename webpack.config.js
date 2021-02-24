1cdb5492fd2b52f4383bc9e800587d1e5c42d333
Asthriona <im@asthriona.com>
2021-02-24 17:00:49 +0100

    add pwa

--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,1 @@
-new webpack.DefinePlugin({ 'process.env.NODE_ENV': '"development"' })
-Vue.config.devtools = process.env.NODE_ENV === 'development';
+Vue.config.devtools = process.env.NODE_ENV === "development";
