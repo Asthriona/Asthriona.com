c300f32a174098ae9672425df3b229a6b05584aa
Asthriona <im@asthriona.com>
2021-04-07 17:06:18 +0200

    add Nishikino network bar

--- src/components/Nishikino.vue
+++ src/components/Nishikino.vue
@@ -1,20 +1,21 @@
 <template>
   <div>
-    <!-- <nav>
+     <nav>
             <ul class="text-left">
                 <li><b-img src="https://asthriona.s3.fr-par.scw.cloud/ShareX/2021/02/Nishikino_Maki_ALL_STARS_24x24.png"></b-img>&nbsp;</li>&nbsp;
                 <li>Nishikino Networks</li> - 
-                <li> <a href="http://" target="_blank" rel="noopener noreferrer"> <b-img src="https://www.asthriona.com/android-chrome-192x192.png" width="20px"></b-img> Asthriona.com </a></li> - 
-                <li> <a href="http://" target="_blank" rel="noopener noreferrer"> <b-img src="https://yukiko.app/img/ico/android-chrome-192x192.png" width="20px"></b-img> Yukiko </a></li> - 
-                <li> <a href="http://" target="_blank" rel="noopener noreferrer"> <b-img src="https://pbs.twimg.com/profile_images/1072650942246518784/b22PO7U9_400x400.jpg" width="20px"></b-img> AnimeFM </a></li> - 
-                <li> <a href="http://" target="_blank" rel="noopener noreferrer"> <b-img src="https://pbs.twimg.com/profile_images/723714544221208580/VhITva-V_400x400.jpg" width="20px"></b-img> Cloudsdale Radio </a></li>
+                <li> <a href="https://Asthriona.com" target="_blank" rel="noopener noreferrer"> <b-img src="https://cdn.nishikino.xyz/asthriona/icons/android-chrome-192x192.png" width="20px"></b-img> Asthriona.com </a></li> - 
+                <li> <a href="https://Yukiko.app" target="_blank" rel="noopener noreferrer"> <b-img src="https://yukiko.app/img/ico/android-chrome-192x192.png" width="20px"></b-img> Yukiko </a></li> - 
+                <!-- <li> <a href="https://AnimeFM.co" target="_blank" rel="noopener noreferrer"> <b-img src="https://pbs.twimg.com/profile_images/1072650942246518784/b22PO7U9_400x400.jpg" width="20px"></b-img> AnimeFM </a></li> -  -->
+                <li> <a href="https://RainboomStudio.com" target="_blank" rel="noopener noreferrer"> <b-img src="https://pbs.twimg.com/profile_images/1174208080164327424/beWGObS1_400x400.jpg" width="20px"></b-img> Rainboom Studio </a></li> - 
+                <li> <a href="https://Cloudsdaleradio.com" target="_blank" rel="noopener noreferrer"> <b-img src="https://pbs.twimg.com/profile_images/723714544221208580/VhITva-V_400x400.jpg" width="20px"></b-img> Cloudsdale Radio </a></li>
             </ul>
-            <ul class="right">
+            <!-- <ul class="right">
                 <li>Friends Websites: </li>
                 <li>woops </li> - 
                 <li>CodeMiko </li> 
-            </ul>
-        </nav> -->
+            </ul> -->
+        </nav> 
   </div>
 </template>
 
