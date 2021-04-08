6291413dd33be62df7345bdd3cf7de74f9a1db6b
Asthriona <im@asthriona.com>
2021-04-08 11:43:18 +0200

    remove font awesome

--- src/main.js
+++ src/main.js
@@ -4,9 +4,6 @@ import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 Vue.config.productionTip = false;
 
@@ -16,8 +13,6 @@ import './registerServiceWorker'
 Vue.use(require('vue-moment'));
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
-Vue.component('font-awesome-icon', FontAwesomeIcon)
-library.add(faUserSecret)
 
 new Vue({
     router,
