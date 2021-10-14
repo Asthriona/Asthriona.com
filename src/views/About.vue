a27330b7024f1276cb9e6a697aa4029a1ee5977f
Asthriona <im@asthriona.com>
2021-10-14 07:05:23 +0200

    Fix typo on about

--- src/views/About.vue
+++ src/views/About.vue
@@ -39,7 +39,7 @@
             <tbody>
               <tr>
                 <td>Name:</td>
-                <td>Makoto "Astdriona" Ng</td>
+                <td>Makoto "Asthriona" Ng</td>
               </tr>
               <tr>
                 <td>Included features:</td>
@@ -123,12 +123,12 @@
                 <th>Front End</th>
                 <th>Vue.js 3, Webpack, Boostrap Vue, core-js</th>
               </tr>
+              <tr></tr>
               <tr>
-                <tr>
                 <th>Hosting</th>
                 <th>Cloudflare, Nishikino Network</th>
               </tr>
-              <tr>
+              <tr></tr>
               <tr>
                 <th>Back End</th>
                 <th>Node.js, Express, MongoDB, Mongoose</th>
@@ -148,142 +148,6 @@
             </tbody>
           </table>
         </b-col>
-        <!-- <b-col>
-          <h1>About me.</h1>
-          <p>
-            Hey! Welcome to my blog! Im <b>Asthriona</b>, Some sort of WebDev, I
-            make code and sometimes it works for some obscure reason. <br />
-            I'm also the founder of <i>Yukiko Dev Team</i> we are the creator of
-            Yukiko a fancy little Discord bot, That will be more than happy to
-            help you manage your Discord guild if needed! <br />
-            Our goal with Yukiko is to being able to make a whole guild with
-            just one bot that do pretty much everything you could need. from
-            moderation to fun commands! if you want to try it head to
-            <a
-              href="https://yukiko.app"
-              target="_blank"
-              rel="noopener noreferrer"
-              >Yukiko.app</a
-            >! <br />
-            Im also working as Lead Developper for
-            <a
-              href="https://rainboomStudio.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              >Rainboom Studio</a
-            >
-            an indi game dev Studio.
-            <br />
-
-            Outside of code and work, i love to have fun with my &#60;Eternal
-            Vengeance &#62;, watch anime or play Whatever i can, lately mostly
-            Valorant. You can find
-            <a
-              href="https://worldofwarcraft.com/en-gb/character/eu/magtheridon/m%C3%A2koto"
-              target="_blank"
-              rel="noopener noreferrer"
-              >my Anilist here</a
-            >
-            (Yes Im a weeb.) You can find my
-            <a
-              href="https://anilist.co/user/Asthriona/"
-              target="_blank"
-              rel="noopener noreferrer"
-              >Main WoW Character here</a
-            >. If you wanna hangout and talk games, or anime I'll be more than
-            happy to do so, My fav games are WoW, Persona (4/5) and Valorant I
-            guess? Anime side My top 3 is something like... Angel Beats!,
-            Charlotte, Steins;gate
-          </p>
-          <h1>Mental Health</h1>
-          <p>
-            Mental Health is a really important topic for me, No place for
-            making fun of it. I personally know hos it is to feel like trash for
-            weeks, month, years, as I even to this day I'm suffering of
-            depressions and server Social Anxiety. if you need anything, Im
-            allways availiable on Twitter and Discord.
-          </p>
-          <h1>About this website</h1>
-          <p>
-            This website is developped and hosted by
-            <a
-              href="http://asthriona.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              >Asthriona</a
-            >
-            <br />
-            This is a beta version. if you find bugs, please contact me at
-            <a href="mailto:ping@asthriona.com">ping@asthriona.com</a> or on
-            twitter
-            <a
-              href="https://twitter.com/asthriona"
-              target="_blank"
-              rel="noopener noreferrer"
-              >@Asthriona</a
-            >
-
-            The content of this blog is under © Copyright Asthriona, All Right
-            Reserved. 2021. The code is under
-            <a
-              href="https://github.com/Asthriona/Ashtriona-Blog/blob/master/LICENSE"
-              target="_blank"
-              rel="noopener noreferrer"
-              >Mozilla Public License 2.0</a
-            >
-            which means you can use & modify the code for yourself, but you have
-            to credit me. All credit on images or videos goes directly to their
-            creator. We are using different technologies such as NodeJS,
-            Express, MongoDB & apache. The code is availiable on GitHub.
-          </p>
-          <h1>Sources:</h1>
-          <ul>
-            <li>— NodeJS</li>
-            <li>— VueJS</li>
-            <li>— MongoDB</li>
-            <li>— Boostrap</li>
-            <li>— AshquiRenee Web Server</li>
-            <li>— Google Font</li>
-          </ul>
-          <h1>Credit:</h1>
-          <p>
-            Almost anything on this website has been done by myself, (Art, Code,
-            Article) if something do not belong to me, all credit goes to their
-            respoective artist/owner. They will allways be credited at the
-            bottom of the page or at the end of an article if applicable.
-          </p>
-          <h3>Code Credit:</h3>
-          <p>
-            Most of the footer has been done my
-            <a
-              href="http://mangoya.cn/"
-              target="_blank"
-              rel="noopener noreferrer"
-              >Aimee</a
-            >
-            they also are a big inspiration sources for the look and some
-            feature of this blog. if you enjoy this blog or are using this front
-            for yourself please go give them a heart &#60;3
-          </p>
-          <h3>Art Credit:</h3>
-          <p>
-            Most of the jett Artwork on the landing page belong to
-            <a
-              href="https://www.riotgames.com/en"
-              target="_blank"
-              rel="noopener noreferrer"
-              >Riot Games.</a
-            >
-            The red/lowpoly one has been made by
-            <a
-              href="https://twitter.com/greenp_94"
-              target="_blank"
-              rel="noopener noreferrer"
-              >GreenP</a
-            >
-            and is a commission they made for me.
-          </p>
-        </b-col> -->
       </b-row>
     </b-container>
   </div>
