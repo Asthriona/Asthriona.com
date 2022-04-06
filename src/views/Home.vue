6ed639f700faf704c70d33543bd9c3ee9fef0932
Asthriona <im@asthriona.com>
2022-04-06 03:36:20 +0200

    chore: remove: 3D, RandomBG, useless code, add: static bg

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,11 @@
 /* eslint-disable */
 <template>
   <div class="div">
-    <b-container
-      class="h-100"
-      v-bind:style="{
-        'background-image': 'url(' + backgroundImage.backgroundImage + ')'
-      }"
-      fluid
-    >
-      <!-- <video autoplay muted loop id="bgvids" src="../assets/agent-background-generic.mp4"></video> -->
+    <b-container class="h-100" fluid>
       <div class="masterhead">
         <b-row class="vh-100" align-v="center">
           <div class="headinStuff">
-            <h1 class="tridi">Asthriona</h1>
+            <h1 class="logo">Asthriona</h1>
             <br />
             <b-col cols="12" class="text-center">
               <h3 class="void" v-if="AsthrionaIsDead == false">
@@ -23,7 +16,7 @@
                 <span style="color:red;">mismatch</span>.
               </h3>
             </b-col>
-            <h3 class="tridi2">
+            <h3 class="snsHeader">
               <a
                 href="https://twitter.com/asthriona"
                 target="_blank"
@@ -75,6 +68,7 @@
             class="custom-select"
             v-model="lang"
             @change="handleChange(e)"
+            disabled
           >
             <option value="ja">日本語</option>
             <option value="en">English</option>
@@ -297,12 +291,12 @@ export default {
       lang: localStorage.getItem("lang") || "en",
       AsthrionaIsDead: false,
       random: [
-        // "/*This line is disabled*/",
+        "/*This line is disabled*/",
         // "死にたい",
         // "空に消えたい",
         // "Better keep it “just me”, It’s better that way, trust me",
         // "it’s Me, Myself and I again",
-        // "Hello from Speedtest.net!",
+        "Hello from Speedtest.net!",
         "Welcome to 2020 Season 3!",
         "You can now play 2021 as luigi",
         "Click here to cancel your subscription to 2022"
@@ -327,6 +321,7 @@ export default {
   },
   methods: {
     handleChange(e) {
+      e.preventDefault();
       localStorage.setItem("lang", e.target.value);
       window.location.reload();
     },
@@ -340,15 +335,15 @@ export default {
         });
       }, 10000);
     }
-  },
-  computed: {
-    backgroundImage() {
-      var bg = Math.floor(Math.random() * 6) + 1;
-      return {
-        backgroundImage: `${this.allBackgroundImages[bg]}`
-      };
-    }
   }
+  // computed: {
+  //   backgroundImage() {
+  //     var bg = Math.floor(Math.random() * 6) + 1;
+  //     return {
+  //       backgroundImage: `${this.allBackgroundImages[bg]}`
+  //     };
+  //   }
+  // }
 };
 </script>
 
@@ -357,34 +352,32 @@ export default {
   font-family: Asthriona;
   src: url(../assets/font/logofont.ttf) format("truetype");
 }
-.tridi {
-  text-align: center;
-  font-family: "Asthriona";
-  font-size: 5.4em;
-  color: #f5f5f5;
-  text-shadow: 1px 1px 1px #919191, 1px 2px 1px #919191, 1px 3px 1px #919191,
-    1px 4px 1px #919191, 1px 5px 1px #919191, 1px 6px 1px #919191,
-    1px 7px 1px #919191, 1px 8px 1px #919191, 1px 9px 1px #919191,
-    1px 10px 1px #919191, 1px 18px 6px rgba(16, 16, 16, 0.4),
-    1px 22px 10px rgba(16, 16, 16, 0.2), 1px 25px 35px rgba(16, 16, 16, 0.2),
-    1px 30px 60px rgba(16, 16, 16, 0.4);
+.masterhead {
+  background-image: url(https://cdn.asthriona.com/rplaceHoloBackground.jpg);
+  background-size: cover;
 }
-.tridi2 {
+.logo {
+  font-family: Asthriona;
+  font-size: 7em;
+  color: #fff;
+  text-shadow: 0 0 7px #000;
   text-align: center;
-  font-family: "Asthriona";
+  margin-top: -1em;
+}
+.snsHeader {
+  font-family: Asthriona;
   font-size: 2em;
-  color: #f5f5f5;
-  text-shadow: 1px 1px 1px #919191, 1px 2px 1px #919191, 1px 3px 1px #919191,
-    1px 4px 1px #919191, 1px 5px 1px #919191, 1px 6px 1px #919191,
-    1px 7px 1px #919191, 1px 8px 1px #919191, 1px 9px 1px #919191,
-    1px 10px 1px #919191, 1px 18px 6px rgba(16, 16, 16, 0.4),
-    1px 22px 10px rgba(16, 16, 16, 0.2), 1px 25px 35px rgba(16, 16, 16, 0.2),
-    1px 30px 60px rgba(16, 16, 16, 0.4);
+  color: #fff;
+  text-shadow: 0 0 10px #000;
+  text-align: center;
+  margin-top: -1em;
 }
 .headinStuff {
   width: 100%;
 }
 .void {
+  position: relative;
+  top: -1.5em;
   width: 100%;
   font-size: 2rem;
   color: #f5f5f5;
@@ -410,53 +403,6 @@ li {
   color: #fff;
   font-size: 1.2rem;
 }
-.main-header {
-  width: 100% !important;
-  background: #38529c !important;
-  height: 4.5rem !important;
-  position: absolute;
-  top: 100%;
-}
-
-.main-nav {
-  margin-bottom: 4.2rem;
-  height: 100% !important;
-}
-
-.nav-link {
-  list-style: none !important;
-  margin: 0 !important;
-  padding: 0 !important;
-  display: flex !important;
-  justify-content: center !important;
-  align-items: center !important;
-  height: 100% !important;
-  margin: 0 1rem !important;
-  padding: 0.3rem !important;
-}
-
-.nav-links {
-  height: 100% !important;
-  display: flex !important;
-  justify-content: center !important;
-  align-items: center !important;
-  margin: 0 1rem !important;
-  padding: 0.3rem !important;
-}
-
-.nav-link.nav-link.nuxt-link-exact-active {
-  border-bottom: 3px solid #06c4d1 !important;
-}
-
-.nav-links a {
-  display: block !important;
-  text-decoration: none !important;
-  color: #fff !important;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983 !important;
-}
 .content {
   margin-top: 2rem;
 }
@@ -474,19 +420,5 @@ li {
 }
 .spotify a:active {
   color: #cccccc;
-}
-.column {
-  float: left;
-  width: 20%;
-  margin-bottom: 16px;
-  padding: 0 8px;
-}
-#bgvids {
-  z-index: -1;
-  position: absolute;
-  right: 0;
-  bottom: 0;
-  min-width: 100%;
-  min-height: 100%;
 }
 </style>
