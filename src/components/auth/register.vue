37077616e46c6f40070f3434b8b8eebaef18f80e
Makoto Kobayashi <im@asthriona.com>
2022-09-16 15:02:45 +0200

    refactor: :sparkles: Re-enable login with new page and v2 api.

--- src/components/auth/register.vue
+++ src/components/auth/register.vue
@@ -1,115 +1,134 @@
 <template>
-  <div>
-    <!-- <b-container>
-      <b-row>
-        <b-col cols="3"></b-col>
-        <b-col>
-          <div v-if="error">
-            <b-alert show variant="danger">
-              <h4 class="alert-heading">Well done!</h4>
-              <p>Aye! An error successfuly happened! <br /></p>
-              <hr />
-              <p class="mb-0">
-                {{ error.message }}
-              </p>
-            </b-alert>
-          </div>
-          <div v-if="success">
-            <b-alert show variant="success">
-              <h4 class="alert-heading">Aye!</h4>
-              <p>
-                Your account {{ success }} has been created! <br />
-                We sent you an email to verify your address, click the link and
-                you should be able to login!
-              </p>
-            </b-alert>
-          </div>
-          <b-form @submit="onSubmit" @reset="onReset">
-            <label for="text-username">Username</label>
-            <b-form-input
-              id="username"
-              label="username"
-              v-model="form.username"
-              description="Enter your username"
-              placeholder="Username"
-            ></b-form-input>
-            <label for="text-username">Email</label>
-            <b-form-input
-              id="email"
-              label="email"
-              v-model="form.email"
-              description="Enter your email"
-              placeholder="Email"
-            ></b-form-input>
-            <label for="text-password">Password</label>
-            <b-form-input
-              type="password"
-              id="text-password"
-              v-model="form.password"
-              aria-describedby="password-help-block"
-              placeholder="Password"
-            ></b-form-input>
-            <b-form-text id="password-help-block">
-              Your password must be 8-20 characters long, can contain letters
-              and numbers, special characters and emoji.
-            </b-form-text>
-            <b-button type="submit" variant="primary">Submit</b-button>
-            <b-button type="reset" variant="danger" class="resetBtn"
-              >Reset</b-button
-            >
-          </b-form>
-        </b-col>
-        <b-col cols="3"></b-col>
-      </b-row>
-    </b-container> -->
-    <center>
-      <h1>Registration are temporary disabled from this website.</h1>
-      <h2>
-        Please head to <a href="https://asthriona.space">Asthriona.space</a> for
-        now.
-      </h2>
-    </center>
-  </div>
+  <v-container>
+    <v-row align="center" justify="center">
+      <v-col xl="5" xs="12" v-if="alert.type">
+        <v-alert :type="alert.type">
+          <p>{{ alert.message }}</p>
+        </v-alert>
+      </v-col>
+      <v-col cols="12" v-if="alert.type"></v-col>
+      <v-col xl="5" xs="12">
+        <v-form ref="form" v-model="valid" lazy-validation>
+          <v-text-field
+            v-model="displayName"
+            :counter="20"
+            :rules="[rules.required, rules.min, rules.max]"
+            label="Display Name"
+            require
+          ></v-text-field>
+          <v-text-field
+            v-model="username"
+            :counter="10"
+            :rules="[rules.required, rules.min, rules.max]"
+            label="Username"
+            require
+          ></v-text-field>
+          <v-text-field
+            v-model="email"
+            :rules="[rules.required, rules.email]"
+            label="Email"
+            require
+          ></v-text-field>
+          <v-text-field
+            v-model="password"
+            :counter="50"
+            :rules="[rules.required, rules.min, rules.password]"
+            type="password"
+            label="Password"
+            attr="new-password"
+            require
+          ></v-text-field>
+          <!-- <v-text-field
+            v-model="passwordVerification"
+            :counter="50"
+            :rules="[rules.required, rules.passwordVerification]"
+            type="password"
+            label="Password Again..."
+            attribute="new-password"
+            require
+          ></v-text-field> -->
+          <v-checkbox v-model="terms">
+            <template v-slot:label>
+              <div @click.stop="">
+                I accept the Terms of Service, and I have read the Privacy
+                policy
+              </div>
+            </template>
+          </v-checkbox>
+          <v-btn color="primary" @click="register" :disabled="!valid && !terms">
+            Register
+          </v-btn>
+        </v-form>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import axios from "axios";
+const axios = require("axios");
 export default {
+  name: "register",
   data() {
     return {
-      error: "",
-      success: "",
-      form: {
-        username: "",
-        email: "",
-        password: ""
-      }
+      displayName: "",
+      username: "",
+      email: "",
+      password: "",
+      passwordVerification: "",
+      terms: false,
+      valid: false,
+      rules: {
+        required: v => !!v || "Required",
+        min: v => (v && v.length >= 3) || "Must be at least 3 characters",
+        max: v => (v && v.length <= 20) || "Must be less than 20 characters",
+        email: value =>
+          (value &&
+            /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value)) ||
+          "Invalid email",
+        password: value =>
+          (value &&
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
+              value
+            )) ||
+          "Password must contain at least one lowercase letter, one uppercase letter, one number, and one special character",
+        passwordVerification: v =>
+          v !== this.password || "Passwords do not match"
+      },
+      alert: {
+        message: "",
+        type: ""
+      },
+      success: ""
     };
   },
   methods: {
-    onSubmit(event) {
-      event.preventDefault();
-      axios.post(process.env.VUE_APP_URI + "/auth/register", this.form).then(
-        response => {
-          this.success = response.data.username;
-        },
-        err => {
-          this.error = err.response.data;
-        }
-      );
-    },
-    onReset(event) {
-      event.preventDefault();
-      // Reset our form values
-      this.form.username = "";
-      this.form.password = "";
-      this.show = false;
-      this.$nextTick(() => {
-        this.show = true;
-      });
+    register() {
+      if (this.valid) {
+        axios
+          .post(`${process.env.VUE_APP_URI}login/register`, {
+            displayName: this.displayName,
+            username: this.username,
+            email: this.email,
+            password: this.password,
+            passwordVerification: this.passwordVerification,
+            terms: this.terms
+          })
+          .then(res => {
+            this.alert.type = "success";
+            this.alert.message =
+              "You are now logged in, you will be redirected in a second.";
+            setTimeout(() => {
+              this.$router.push("/");
+              return res;
+            }, 2000);
+          })
+          .catch(err => {
+            this.alert.message = err.response.data.message;
+            this.alert.type = "error";
+          });
+      }
     }
-  },
-  mounted() {}
+  }
 };
 </script>
 
