ef5568df00ccd92c615b29b713ac879131d43fb8
Makoto Kobayashi <im@asthriona.com>
2023-05-09 03:15:40 +0200

    ci: :green_heart: Update ESlint config bypass error

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
-  },
+  }
 
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"]
+  // extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"]
 };
