b6248f922d462538abd87000e0e503d15826d36d
Makoto Kobayashi <im@asthriona.com>
2023-05-09 11:48:03 +0200

    ci: :green_heart: Remove ESkint dup key

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,4 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
   }
-
-  // extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"]
 };
