459f335ef053cd046a1f1149bf122c2c5dec10e3
Asthriona <im@asthriona.com>
2024-08-24 11:02:12 +0200

    Update home page texts

--- pages/index.vue
+++ pages/index.vue
@@ -8,16 +8,16 @@
         <div class="md:w-2/4">
             <h2 class="text-3xl font-bold mt-8">Who am I?</h2>
             <p class="text-lg py-2">
-                I'm Asthriona, You can call me <i class="text-white">Makoto</i>, but most of my English speaking friends call me "<i class="text-white">Mako</i>".
+                I'm Asthriona, You can call me <i class="text-white">Makoto</i>, but most of my English speaking friends call me "<i class="text-white">Mako</i>" (except TheJudge who likes to call me "Mako-san").
                 I'm a Full-Stack JS developper, who like to pick-up any sorts of skills, such as anything related to
-                cars, and computer science.
+                cars, and computer science, lock picking, Video Games, whatever.
                 As a programer, I'm self-taught. I went to school for networking and datacenter management.
             </p>
             <p class="text-lg py-2">
                 I have other hobbies and things I like, yet again obviously cars, since this is a very wide subject, but
                 also anime, music, I can also enjoy some fine TV Shows and movies.
                 Meaning that I make this website as a hobby and for simple online presence, I really like the
-                "<i class="text-white">old-school</i>" internet, whene it was filled with small blogs,
+                "<i class="text-white">old-school</i>" internet, when it was filled with small blogs,
                 and places to share our daily lives and hobbies. In here I might share programing tips, Anime/TV Show/Movies review,
                 but also try to keep the "project" page updated with my latests
                 project, and dev-logs.
