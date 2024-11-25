8a37197f15d4b64952c020655c87f1cf9e219909
Asthriona <im@asthriona.com>
2024-11-25 08:20:34 +0100

    refactor(Template): :zap: Move all images to netlify CDN
    So I can improve the performance of the whole website even when loaded from the other side of the world. aint that cool?! i think it is.
    
    BREAKING: Images? maybe? works in dev.

--- components/TheNavBars.vue
+++ components/TheNavBars.vue
@@ -13,7 +13,7 @@
           </div>
           <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div class="flex flex-shrink-0 items-center">
-              <a href="/"><img class="h-8 w-auto" src="https://cdn.asthriona.com/static/Asthriona-SL.svg" alt="Asthriona ltd." /></a>
+              <a href="/"><NuxtImg format="webp"  quality="40" class="h-8 w-auto" src="https://cdn.asthriona.com/static/Asthriona-SL.svg" alt="Asthriona ltd." /></a>
             </div>
             <div class="hidden sm:ml-6 sm:block">
               <div class="flex space-x-4">
