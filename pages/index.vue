852f8475fb84f1e0ce206df3e60b2d77802b2c74
Asthriona <im@asthriona.com>
2026-02-20 13:41:41 +0100

    Fix image link

--- pages/index.vue
+++ pages/index.vue
@@ -25,7 +25,7 @@
                 project, and dev-logs.
             </p>
         </div>
-        <NuxtImg src="https://cdn.asthriona.com/static/2026_pfp.jp" alt="Asthriona's Avatar, a female character with deep purple hairs, one eye is hidden by the hairs, and she has long pointy ears." height="384" width="384"
+        <NuxtImg src="https://cdn.asthriona.com/static/2026_pfp.jpg" alt="Asthriona's Avatar, a female character with deep purple hairs, one eye is hidden by the hairs, and she has long pointy ears." height="384" width="384"
             fit="cover" format="webp" quality="80" class="w-1/2 h-1/2 md:max-w-sm p8 mx-auto rounded" />
     </section>
     <section>
@@ -48,7 +48,7 @@ useSeoMeta({
     ogTitle: 'Asthriona',
     description: 'Welcome to my personal Website! Click to learn more!',
     ogDescription: 'Welcome to my personal Website! Click to learn more!',
-    ogImage: 'https://cdn.asthriona.com/static/2026_pfp.jp',
+    ogImage: 'https://cdn.asthriona.com/static/2026_pfp.jpg',
     twitterCard: 'summary_large_image',
 })
 // The metadata stuff for ranking and whatnot. 
